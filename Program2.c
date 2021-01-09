e895851956b78dff6a7eab2b2784860fcbd4bea3
PratikshaD30 <76963523+PratikshaD30@users.noreply.github.com>
2021-01-09 20:08:38 +0530

    Add files via upload

---
+++ Program2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+
+int main()
+{
+	char cLetter = 'A';
+	for(cLetter='A' ; cLetter<='Z' ; cLetter++)
+{
+	printf("%d\n",cLetter);
+}
+
+printf("\n");
+printf("*****End Of Program*****");
+
+return 0;
+		
+
+}
+
+
+
+
+
+
+
+
+#include<stdio.h>
+
+int main()
+
+{
+	char cLetter = 'A';
+	for(cLetter='A'; cLetter<'Z'; cLetter++)
+	{
+	int ival=0;
+	ival = cLetter+1;
+	printf("%c\n",ival);
+	}
+printf("\n");
+printf("****End Of Program****\n");
+return 0;
+}
