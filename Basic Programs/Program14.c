cd334255aaf35e7d027ca7049455ae22ad2c6ca0
PratikshaD30 <76963523+PratikshaD30@users.noreply.github.com>
2021-01-12 00:32:39 +0530

    Create Program14.c

---
+++ Basic Programs/Program14.c
@@ -0,0 +1,32 @@
+
+
+#include<stdio.h>
+
+
+void Accept(int iNo)
+{
+	int iCnt = 0;
+
+	for(iCnt = 1; iCnt <= iNo; iCnt++)
+	{
+	printf("*");
+	}
+
+
+}
+
+
+
+int main()
+{
+
+	int iValue = 0;
+
+	printf("Enter any number:\n");
+	scanf("%d",&iValue);
+
+ 	Accept(iValue);
+
+
+ return 0;
+}
