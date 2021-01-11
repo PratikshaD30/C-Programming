e3561be5e3f96429bb82255023ec6445507bdbb7
PratikshaD30 <76963523+PratikshaD30@users.noreply.github.com>
2021-01-12 03:07:03 +0530

    Create Program18.c

---
+++ Basic Programs/Program18.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+char Alphabet(char);
+
+int main()
+
+{
+	char cLetter = '\0';
+
+	printf("Please enter any Alphabet\n");
+	scanf("%c",&cLetter);
+	
+	Alphabet(cLetter);
+}
+
+char Alphabet(char cAlpha)
+{
+
+	
+	if((cAlpha>='a') && (cAlpha<='z'))
+	{
+
+	
+	 printf("Given Alphabet in uppercase is : %c\n",cAlpha -32);
+	
+	}
+	
+	else if((cAlpha>='A') && (cAlpha<='Z'))
+
+	{
+		
+		printf("Given Alphabet in lowercase is : %c\n",cAlpha +32);
+	}
+
+	else
+
+	{
+		printf("Please enter valid character\n");	
+	}
+	
+	 
+
+}
